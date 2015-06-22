d95ca1e04201e80bc4ef00a26c4db54759aaa6cc
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-22 16:43:01 +0800

    update install theme script, run npm install auto

--- scripts/install-theme.sh
+++ scripts/install-theme.sh
@@ -6,4 +6,7 @@ remote=$2
 cd $THEMEDIR
 git clone $remote $name
 
+echo "installing the npm dependencies for $name"
+cd $name && npm install --production
+
 echo "$name installed at $THEMEDIR/$name"
