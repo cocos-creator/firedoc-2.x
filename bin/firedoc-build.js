a2ef43cc412623c18d86a839efb120311a3bc676
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-25 00:53:06 +0800

    start refactoring builder

--- bin/firedoc-build.js
+++ bin/firedoc-build.js
@@ -4,12 +4,19 @@ var program = require('commander');
 var Firedoc = require('../lib/firedoc').Firedoc;
 
 program
+  .option('-l --lint', 'lint the parser')
+  .option('--parse-only', 'only parse')
   .option('-m --markdown', 'generate markdown docs')
   .option('-t --theme <dir>', 'specify theme directory')
-  .option('-o --out <dir>', 'the destination folder to build')
+  .option('-d --dest <dir>', 'the destination folder to build')
   .parse(process.argv);
 
 var doc = new Firedoc({
-  path: program.args[0]
+  path: program.args[0],
+  lint: program.lint,
+  parseOnly: program.parseOnly,
+  markdown: program.markdown,
+  dest: program.dest,
+  theme: program.theme
 });
 doc.build();
