4c31da890678545bdc65bc7e1a27346be6f19b3c
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-25 21:06:31 +0800

    update preview

--- bin/firedoc-preview.js
+++ bin/firedoc-preview.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
 
+var debug = require('debug');
 var program = require('commander');
+var Firedoc = require('../lib/firedoc').Firedoc;
 
 program
+  .option('-l --lint', 'lint the parser')
+  .option('--parse-only', 'only parse')
   .option('-m --markdown', 'generate markdown docs')
   .option('-t --theme <dir>', 'specify theme directory')
-  .option('-o --out <dir>', 'the destination folder to build')
+  .option('-d --dest <dir>', 'the destination folder to build')
+  .option('-L --lang <language>', 'the i18n language')
+  .option('-v --verbose', 'print all verbose information')
   .parse(process.argv);
 
+if (program.verbose) {
+  debug.enable('firedoc:*');
+}
+
+var doc = new Firedoc({
+  path: program.args[0],
+  lint: program.lint,
+  parseOnly: program.parseOnly,
+  markdown: program.markdown,
+  dest: program.dest,
+  lang: program.lang,
+  theme: program.theme
+});
+doc.build();
