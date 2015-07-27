ce4166fb9adf86b3fe312d60974a9a3dcdde1a49
Yazhong Liu <yorkiefixer@gmail.com>
2015-07-27 17:44:12 +0800

    update gh-pages

---
+++ assets/js/api-list.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+
+  'use strict';
+
+  var movedItems = [];
+  var itemsCont = $('#api-items');
+  var itemsList = $('#api-items>li>.api-list-item-clickable');
+  itemsList.on(
+    'click',
+    function onclick () {
+      $(this).parent().find('.members-list').toggleClass('hidden');
+    }
+  );
+
+  var inputing = false;
+  var searchBox = $('#api-tabview-filter>input[type=search]');
+  searchBox.on(
+    'input',
+    function (e) {
+      clearTimeout(inputing);
+      inputing = setTimeout(function () {
+        filter($(this).val());
+      }.bind(this), 300);
+    }
+  );
+
+  $(document).on('keyup', function (e) {
+    // shortcut 's' -> search
+    if (e.keyCode === 83) {
+      searchBox.focus();
+    }
+  });
+
+  var records = [];
+  itemsCont.find('.module').each(
+    getAppendor('module')
+  );
+  itemsCont.find('.class').each(
+    getAppendor('class')
+  );
+  itemsCont.find('.enum').each(
+    getAppendor('enum')
+  );
+
+  var firstRecords = records.filter(function (r) {
+    return r.type !== 'member';
+  });
+
+  function getAppendor (type) {
+    return function () {
+      var $this = $(this);
+      var name = $this.text().trim().replace(/\n\r/, '');
+      records.push(
+        appendMmebers({
+          'this': $this.parent().parent(),
+          'ns': name.toLowerCase(),
+          'name': name,
+          'type': type
+        })
+      );
+    };
+  }
+
+  function appendMmebers (parent) {
+    $(parent.this).find('.members>li').each(function () {
+      var $this = $(this);
+      var $ul = $this.parent('ul');
+      var $h3 = $ul.prev('h3');
+      var name = $this.find('.name').text().trim().replace(/\n\r/, '');
+      var namespace = [parent.name, name].join('.').toLowerCase();
+      records.push({
+        'parent': parent,
+        'ns': namespace,
+        'name': name,
+        'type': 'member',
+        'this': $this,
+        '$ul': $ul,
+        '$h3': $h3
+      });
+    });
+    return parent;
+  }
+
+  function clearMoved () {
+    var reset = movedItems.shift();
+    if (reset) {
+      reset();
+      clearMoved();
+    }
+  }
+
+  function filter (keyword) {
+    clearMoved();
+    records.filter(function (r) {
+      r.this.show().addClass('visible');
+      if (r.ns.search(keyword.toLowerCase()) === -1) {
+        r.this.hide().removeClass('visible');
+      } else {
+        if (keyword) {
+          if (r.type !== 'member') {
+            r.this.find('.members-list').removeClass('hidden');
+            itemsCont.prepend(r.this);
+            // set the reset stuff
+            var index = firstRecords.indexOf(r);
+            movedItems.push(function reset () {
+              if (index > 0)
+                r.this.insertAfter(firstRecords[index - 1].this);
+              else
+                r.this.insertBefore(firstRecords[0].this);
+            });
+          } else {
+            r.parent.this.find('.members-list').removeClass('hidden');
+          }
+        } else {
+          r.this.find('.members-list').addClass('hidden');
+        }
+        return true;
+      }
+    }).map(function (r) {
+      if (r.parent) {
+        r.parent.this.show();
+      }
+      return r;
+    });
+  }
+
+});
---
+++ zh/api.js
@@ -0,0 +1,78 @@
+window.apimeta = {
+  "enums": [],
+  "classes": [
+    {
+      "name": "AST",
+      "namespace": "firedoc.AST",
+      "module": "firedoc",
+      "description": "<p>The AST(Abstract syntax tree) of the comment</p>\n"
+    },
+    {
+      "name": "BuilderContext",
+      "namespace": "firedoc.BuilderContext",
+      "module": "firedoc",
+      "description": "<p>The Builder Context</p>\n"
+    },
+    {
+      "name": "DocParser",
+      "namespace": "firedoc.DocParser",
+      "module": "firedoc",
+      "description": ""
+    },
+    {
+      "name": "DocView",
+      "namespace": "firedoc.DocView",
+      "module": "firedoc",
+      "description": "<p><p>View class borrowed from <a href=\"https://github.com/rgrove/selleck\">Selleck</a>\nThe view class is a <strong><code>handlebars</code></strong> template helper.</p></p>\n"
+    },
+    {
+      "name": "Firedoc",
+      "namespace": "firedoc.Firedoc",
+      "module": "firedoc",
+      "description": "<p><p>Firedoc main class</p></p>\n"
+    },
+    {
+      "name": "Locals",
+      "namespace": "firedoc.Locals",
+      "module": "firedoc",
+      "description": "<p>The Theme Locals</p>\n"
+    },
+    {
+      "name": "ParserContext",
+      "namespace": "firedoc.ParserContext",
+      "module": "firedoc",
+      "description": "<p>The ParserContext</p>\n"
+    }
+  ],
+  "modules": [
+    {
+      "name": "digesters",
+      "namespace": "firedoc.digesters",
+      "module": "firedoc",
+      "description": "<p>A map of the default tag processors, keyed by the\ntag name.  Multiple tags can use the same digester\nby supplying the string name that points to the\nimplementation rather than a function.</p>\n"
+    },
+    {
+      "name": "firedoc",
+      "namespace": "firedoc",
+      "description": "<p>The firedoc module</p>\n"
+    },
+    {
+      "name": "helpers",
+      "namespace": "firedoc.helpers",
+      "module": "firedoc",
+      "description": "<p>The helpers module</p>\n"
+    },
+    {
+      "name": "tags",
+      "namespace": "firedoc.tags",
+      "module": "firedoc",
+      "description": ""
+    },
+    {
+      "name": "utils",
+      "namespace": "firedoc.utils",
+      "module": "firedoc",
+      "description": "<p>Utilities modules</p>\n"
+    }
+  ]
+}
